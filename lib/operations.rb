a2bf0d2534517243463aa6997c6eb7d0cdcacb60
Maria Connie Sanchez <msnchz04@gmail.com>
2020-06-06 01:34:06 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60 
+    true 
+    elsif speed < 40
+    true 
+  else 
+    speed = 50
+    false 
+ end 
 end
 
-
-
-def not_safe?(speed)
-	
+def not_safe?(speed = 50)
+  speed > 60 || speed < 40 ? true : false  
 end
 	
 
